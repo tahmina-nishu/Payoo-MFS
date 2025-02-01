1bcb064c58808d390dc9e144dc6c9e50a0069289
tahmina-nishu <akthertahmina572@gmail.com>
2025-02-01 22:36:57 +0600

    Handled Add Money button and get the added money and pin

---
+++ home.js
@@ -0,0 +1,11 @@
+// Add Money to the account
+
+document.getElementById('add-money-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    // console.log('clicked');
+
+    const addedMoney = document.getElementById('input-add-money').value;
+    // console.log(addedMoney);
+    const inputPin = document.getElementById('input-pin').value;
+    // console.log(inputPin);
+})
--- login.js
+++ login.js
@@ -3,6 +3,8 @@
 document.getElementById('login-btn').addEventListener('click', function(event){
     // Step - 2 :  Prevent default behaviour (reloading browser)
     event.preventDefault(); // eta dile page ta reload hobe na .
+
+    
         //console.log('login button clicked')
 
     // Step - 3 :  Get the phone number
