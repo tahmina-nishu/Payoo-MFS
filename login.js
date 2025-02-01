4c0b3540fae2dc3328b11f3d605566a2910f77ec
tahmina-nishu <akthertahmina572@gmail.com>
2025-02-01 08:31:42 +0600

    validate phone number & pin

--- login.js
+++ login.js
@@ -6,6 +6,18 @@ document.getElementById('login-btn').addEventListener('click', function(event){
         //console.log('login button clicked')
 
     // Step - 3 :  Get the phone number
-    const phoneNumber = document.getElementById('phone-number');
-    
+    const phoneNumber = document.getElementById('phone-number').value;
+    console.log(phoneNumber);
+
+    // Step - 4 :  Get the pin number
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(pinNumber);
+
+        // erpor normally phone & pin server e pathale seta check kore dey. kintu apatoto ekta phone & ekta pin set kore kaj korbo
+
+    // Step - 5 :  validate phone and pin
+    if(phoneNumber === '01846152387' && pinNumber === '1234')
+        alert("You are logged in.");
+    else
+        alert("Wrong phone number or pin.");
 })
