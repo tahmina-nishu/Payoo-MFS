4f6233dd53d1a7d38d4332ce72985b450b9eb5ab
tahmina-nishu <akthertahmina572@gmail.com>
2025-02-02 09:57:57 +0600

    added balance

--- home.js
+++ home.js
@@ -4,8 +4,25 @@ document.getElementById('add-money-btn').addEventListener('click', function(even
     event.preventDefault();
     // console.log('clicked');
 
+    // ------------Get added balance----------
     const addedMoney = document.getElementById('input-add-money').value;
     // console.log(addedMoney);
+
+    
+    // ------------Get the in entered----------
     const inputPin = document.getElementById('input-pin').value;
     // console.log(inputPin);
+
+    if(inputPin === '1234')
+    {
+        
+        // ------------Get current balance----------
+        const currentMoney = document.getElementById('current-amount').innerText;
+        console.log(currentMoney);
+
+        const updatedBalance = Number(currentMoney) + Number(addedMoney);
+        console.log(updatedBalance);
+    }
+    else
+    alert('failed to add money');
 })
